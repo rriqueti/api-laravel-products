98f16dd5954eb6db0122ca3fc217cd5fb7710d59
rriqueti <57726626+rriqueti@users.noreply.github.com>
2024-05-07 14:44:51 -0300

    Add Modules Jobs

---
+++ app/Jobs/ProductJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+// use App\Http\Controllers\ProductServices;
+use App\Http\Controllers\ProductServices;
+use App\Models\Product;
+use App\Http\Requests\ProductRequest;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProductJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(ProductServices $services, ProductRequest $request)
+    {
+        //
+        $this->services = $services;
+        $this->request = $request;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+        $this->services->update($this->request->all());
+    }
+}
